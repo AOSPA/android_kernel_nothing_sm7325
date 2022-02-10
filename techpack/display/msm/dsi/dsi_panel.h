b500eb411ed69912f6d74ccf596a6e45dbd2ed15
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 15:08:04 +0200

    techpack: display: dsi_panel: read FOD dimming LUT
    Change-Id: I23f7914ce0adf6c1942c09212516ed5f6b22d289

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -206,6 +206,12 @@ struct dsi_panel_ops {
 	int (*parse_power_cfg)(struct dsi_panel *panel);
 };
 
+#define BRIGHTNESS_ALPHA_PAIR_LEN 2
+struct brightness_alpha_pair {
+	u16 brightness;
+	u8 alpha;
+};
+
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -271,6 +277,9 @@ struct dsi_panel {
 	u32 tlmm_gpio_count;
 
 	struct dsi_panel_ops panel_ops;
+
+	struct brightness_alpha_pair *fod_dim_lut;
+	unsigned int fod_dim_lut_len;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
